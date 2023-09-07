d58e577fc4a98dfa6d1d17ae6314cee32d8fe2da
Jason <jasonheflinger@gmail.com>
2023-09-06 23:48:54 -0500

    started work on encryption

--- Needles/src/Security/Encryption.cpp
+++ Needles/src/Security/Encryption.cpp
@@ -1,1 +1,29 @@
 #include "ndlpch.h"
+#include "Encryption.h"
+
+namespace Needles {
+	static Seed s_Seed;
+
+	Seed& Encryption::GenerateSeed() {
+		// TODO
+		return s_Seed;
+	}
+
+	Seed& Encryption::GetSeed() {
+		return s_Seed;
+	}
+
+	void Encryption::SetSeed(Seed& seed) {
+		s_Seed = seed;
+	}
+
+	Data& Encryption::Encrypt(Data& data) {
+		// TODO
+		return data;
+	}
+
+	Data& Encryption::Decrypt(Data& data) {
+		// TODO
+		return data;
+	}
+}
--- Needles/src/Security/Encryption.h
+++ Needles/src/Security/Encryption.h
@@ -1,1 +1,23 @@
 #pragma once
+
+namespace Needles {
+	struct Seed {
+		uint64_t Epoch;
+		uint8_t* Data;
+		uint64_t Size;
+	};
+
+	struct Data {
+		uint8_t* Data;
+		uint64_t Size;
+	};
+
+	class Encryption {
+	public:
+		static Seed& GenerateSeed();
+		static Seed& GetSeed();
+		static void SetSeed(Seed& seed);
+		static Data& Encrypt(Data& data);
+		static Data& Decrypt(Data& data);
+	};
+}
