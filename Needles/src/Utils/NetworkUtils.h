5731d8b3bee3303d85959db8f9bdb77091dc4a49
Cameron Lee <60496816+papabeanstalk@users.noreply.github.com>
2023-09-16 08:43:54 -0700

    Update NetworkUtils.h

--- Needles/src/Utils/NetworkUtils.h
+++ Needles/src/Utils/NetworkUtils.h
@@ -1,15 +1,6 @@
 #pragma once
 #include "Core/Structures.h"
 
-#ifdef _WIN32
-#include <winsock2.h>
-#include <ws2tcpip.h>
-#else
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#endif
-
 namespace Needles {
 	class NetworkUtils {
 	public:
